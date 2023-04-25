0c3fe4c7ce93c68745e8eeb4882b30d651747a2c
Victoria Lemay <victoria.lemay.1@ulaval.ca>
2023-04-25 19:32:32 -0400

    fct solve_ampl

--- solver.py
+++ solver.py
@@ -2,20 +2,22 @@ import amplpy
 import os
 import numpy as np
 
-ampl_env = amplpy.Environment()
-ampl = amplpy.AMPL(ampl_env)
+def solve_ampl():
 
-ampl.setOption('solver', 'gurobi')
+    ampl_env = amplpy.Environment()
+    ampl = amplpy.AMPL(ampl_env)
 
-model_dir = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2')
-ampl.read(os.path.join(model_dir, 'tp2_new.mod'))
+    ampl.setOption('solver', 'gurobi')
 
-#importer les data
+    model_dir = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2')
+    ampl.read(os.path.join(model_dir, 'tp2_new.mod'))
+
+    #importer les data
 
 
-ampl.solve()
+    ampl.solve()
 
-print('Objective : {}'.format(ampl.getObjective('couttotal').value()))
-solution = ampl.getVariable('NBUS').getValues()
-print('Solution :\n' + str(solution))
+    print('Objective : {}'.format(ampl.getObjective('couttotal').value()))
+    solution = ampl.getVariable('NBUS').getValues()
+    print('Solution :\n' + str(solution))
 
