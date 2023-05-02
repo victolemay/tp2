a1e64c42968f859fad0658e954e68be52e42afee
Victoria Lemay <victoria.lemay.1@ulaval.ca>
2023-05-02 18:35:49 -0400

    data

--- solver.py
+++ solver.py
@@ -1,29 +1,30 @@
 import amplpy
 import os
 import numpy as np
-
+"""""
 class Solver():
 
     def __init__(self):
         pass
 
 
-    def solve_ampl():
+    def solve_ampl():"""
 
-        ampl_env = amplpy.Environment()
-        ampl = amplpy.AMPL(ampl_env)
+ampl_env = amplpy.Environment()
+ampl = amplpy.AMPL(ampl_env)
 
-        ampl.setOption('solver', 'gurobi')
+ampl.setOption('solver', 'gurobi')
 
-        model_dir = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2')
-        ampl.read(os.path.join(model_dir, 'tp2_new.mod'))
+model_dir = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2')
+data1_dir = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2')
+ampl.read(os.path.join(model_dir, 'tp2_new.mod'))
+ampl.read(os.path.join(data1_dir, 'tp2_new_inst1'))
 
-        #importer les data
+#importer les data
 
+ampl.solve()
 
-        ampl.solve()
-
-        print('Objective : {}'.format(ampl.getObjective('couttotal').value()))
-        solution = ampl.getVariable('NBUS').getValues()
-        print('Solution :\n' + str(solution))
+print('Objective : {}'.format(ampl.getObjective('couttotal').value()))
+solution = ampl.getVariable('NBUS').getValues()
+print('Solution :\n' + str(solution))
 
