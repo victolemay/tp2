9248526065a08433ab414a1c2517c10c4a071c54
Victoria Lemay <victoria.lemay.1@ulaval.ca>
2023-05-04 13:52:36 -0400

    fonction solve

--- solver.py
+++ solver.py
@@ -1,32 +1,32 @@
 import amplpy
 import os
 import numpy as np
-"""""
+
 class Solver():
 
-    def __init__(self, inst):
+    def __init__(self, inst = 'tp2_new_inst1.dat'):
         self.inst = inst
 
     def __str__(self):
         return str('Instance etudiee = ' + str(self.inst))
 
 
-    def solve_ampl():"""
+    def solve_ampl(self):
 
 
-ampl_env = amplpy.Environment()
-ampl = amplpy.AMPL(ampl_env)
+        ampl_env = amplpy.Environment()
+        ampl = amplpy.AMPL(ampl_env)
 
-model_dir = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2')
-data1_dir = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2')
-ampl.read(os.path.join(model_dir, 'tp2_new.mod'))
-ampl.read(os.path.join(data1_dir, 'tp2_new_inst1'))
+        model_dir = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2')
+        data1_dir = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2')
+        ampl.read(os.path.join(model_dir, 'tp2_new.mod'))
+        ampl.read(os.path.join(data1_dir, self.inst))
 
 #importer les data
 
-ampl.solve()
+        ampl.solve()
 
-print('Objective : {}'.format(ampl.getObjective('couttotal').value()))
-solution = ampl.getVariable('NBUS').getValues()
-print('Solution :\n' + str(solution))
+        print('Objective : {}'.format(ampl.getObjective('couttotal').value()))
+        solution = ampl.getVariable('NBUS').getValues()
+        print('Solution :\n' + str(solution))
 
