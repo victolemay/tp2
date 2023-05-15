562a5ee3c8ccb81f01212693e4c755d52be6d8ee
Rosalie18 <rodes230@ulaval.ca>
2023-05-14 22:34:57 -0400

    test ampl

--- mainTest.py
+++ mainTest.py
@@ -1,48 +1,23 @@
 import unittest
-import sys
-
-##Tester si le code est correct
-
-class TestMaFonction(unittest.TestCase):
-    def test_ma_fonction(self):
-        self.assertEqual(ma_fonction(2), 4) ## remplacer ma_fonction par la fonction qu'on veut tester
-        self.assertEqual(ma_fonction(0), 0) ## remplacer ma_fonction par la fonction qu'on veut tester
-        self.assertEqual(ma_fonction(-2), -4) ## remplacer ma_fonction par la fonction qu'on veut tester
-
-if __name__ == 'main':
-    unittest.main()
-##L'exécution de ce code affichera le résultat des tests exécutés. Si tous les tests réussissent, vous verrez "OK" à la fin de la sortie. 
-# Si des tests échouent, vous verrez des messages d'erreur décrivant ce qui a échoué.
-
-##Tester si le code est efficace
-
-# verifier si le temps de résolution est résonable (en bas de 10s)
-
+import os
 import numpy as np
-from solver import minimize
-import time
+from solver import Solver
 
-# Définir la limite de temps
-time_limit = 5 # 5 secondes
+class TestSolver(unittest.TestCase):
 
-# Définir les options du solver
-options = {'maxiter': 1000}
+    def test_solve_ampl(self):
+        solver = Solver('tp2_new_inst1.dat')
+        solver.solve_ampl()
 
-# Définir les contraintes et les bornes
-constraints = {'type': 'ineq', 'fun': constraint}
+        # Vérifier que la solution obtenue est entière
+        solution = solver.get_solution()
+        self.assertTrue(np.allclose(solution, np.round(solution)))
 
-# Définir les valeurs initiales
-x0 = np.array([0.5, 0.5])
+        # Vérifier que la solution est optimale
+        self.assertAlmostEqual(solver.get_objective(), 10510.0, places=1)
 
-# Résoudre le problème en mesurant le temps
-start_time = time.time()
-res = minimize(objective, x0, method='SLSQP', bounds=bounds, constraints=constraints, options=options)
-end_time = time.time()
+        # Vérifier que le nombre de bus utilisés est inférieur ou égal à Maxbus
+        self.assertLessEqual(np.sum(solution), solver.get_max_bus())
 
-# Afficher la solution et le temps de résolution
-if res.success:
-    print("Solution optimale trouvée en {} secondes".format(end_time - start_time))
-else:
-    print("Aucune solution optimale trouvée dans la limite de temps imparti")
-
-
+if __name__ == '__main__':
+    unittest.main()
