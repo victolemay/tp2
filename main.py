69c8d606a78e758d805182044bb20a4a451b76c9
Victoria Lemay <victoria.lemay.1@ulaval.ca>
2023-05-16 15:28:16 -0400

    test pour voir si tout fonctionne

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from solver import Solver_ampl
 from solver import Solver_mz
 
 
-#solver = Solver_ampl('tp2_new_inst1.dat')
-#solver.solve_ampl()
+solver = Solver_ampl('tp2_new_inst1.dat')
+solver.solve_ampl()
 
 solvermz = Solver_mz('inst1.dzn.txt')
 solvermz.solve_mz()
--- solver.py
+++ solver.py
@@ -13,19 +13,17 @@ class Solver_ampl():
     def __str__(self):
         return str('Instance etudiee = ' + str(self.inst))
 
-
     def solve_ampl(self):
 
-        ampl_env = amplpy.Environment('/Applications/ampl_macos643')
+        ampl_env = amplpy.Environment('/Users/victorialemay/Desktop/ampl_macos64_1')
         ampl = amplpy.AMPL(ampl_env)
 
-        model_dir = os.path.normpath('/Users/rosaliedesmeules/Desktop/AMPLSIAD')
-        data1_dir = os.path.normpath('/Users/rosaliedesmeules/Desktop/AMPLSIAD')
+        model_dir = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2')
+        data1_dir = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2')
         ampl.read(os.path.join(model_dir, 'tp2_new.mod'))
 
         #ampl.read(os.path.join(data1_dir, 'tp2_new_inst1.dat'))
         ampl.readData(os.path.join(data1_dir, self.inst))
-
 
         ampl.solve()
 
@@ -55,12 +53,6 @@ class Solver_mz():
         data_path_inst = os.path.join(data_path, self.inst)
 
         instance = minizinc.Instance(solver, problem)
-        #S = enum.Enum('S', ['soir1', 'soir2', 'soir3', 'soir4', 'soir5', 'soir6', 'soir7', 'soir8', 'soir9', 'soir10', 'soir11'])
-        #Z = enum.Enum('Z', ['zone1', 'zone2', 'zone3', 'zone4', 'zone5'])
-        
-        #if self.inst == 'inst1_dzn.txt':
-            #Z = enum.Enum('Z', ['zone1', 'zone2', 'zone3', 'zone4', 'zone5'])
-            
         
         with open(data_path_inst, 'r') as text_file:
             for line in text_file.readlines():
@@ -127,46 +119,13 @@ class Solver_mz():
 
         print('Objectif: ' + str(result['couttotal'])) 
         print('Solution:\n' + str(result['NBUS']))
-""""
-        instance = minizinc.Instance(solver, problem)
-        S = enum.Enum('S', ['soir1', 'soir2', 'soir3', 'soir4', 'soir5', 'soir6', 'soir7', 'soir8', 'soir9', 'soir10', 'soir11'])
-        instance['S'] = S
-        
-        if self.inst == 'inst1_dzn':
-            Z = enum.Enum('Z', ['zone1', 'zone2', 'zone3', 'zone4', 'zone5'])
-            instance['Z'] = Z
-        
-        
-        
-        data = pd.read_csv(data_path_inst, delimiter='=', header=None, names=['param', 'value'])
-
-        placedispo = data.loc[data['param'] == 'placedispo', 'value'].values[0]
-        cfixe = data.loc[data['param'] == 'cfixe', 'value'].values[0]
-        salaire = data.loc[data['param'] == 'salaire', 'value'].values[0]
-        esssence = data.loc[data['param'] == 'essence', 'value'].values[0]
-        vitbus = data.loc[data['param'] == 'vitbus', 'value'].values[0]
-        maxbus = data.loc[data['param'] == 'maxbus', 'value'].values[0]
-        pers = data.loc[data['param'] == 'pers', 'value'].values[0]
-        pourcentage = data.loc[data['param'] == 'pourcentage', 'value'].values[0]
-        distancet = data.loc[data['param'] == 'distancet', 'value'].values[0]
-
-        instance['placedispo'] = placedispo
-        instance['cfixe'] = cfixe
-        instance['salaire'] = salaire
-        instance['essence'] = esssence
-        instance['vitbus'] = vitbus
-        instance['maxbus'] = maxbus
-        instance['pers'] = pers
-        instance['pourcentage'] = pourcentage
-        instance['distancet'] = distancet
-"""
 
         
-        #print(result.status)
-        #print(result.statistics)
-        #if result.status is minizinc.Status.SATISFIED:
-            #print(result['NBUS'])
-        #else:
-            #print('Aucune solution trouvée')
+        print(result.status)
+        print(result.statistics)
+        if result.status is minizinc.Status.OPTIMAL_SOLUTION:
+            print(result['NBUS'])
+        else:
+            print('Aucune solution trouvée')
 
 
