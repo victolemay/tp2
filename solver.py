c9344f7755ab63a509deab336d8e10a4cea483ed
Victoria Lemay <victoria.lemay.1@ulaval.ca>
2023-05-16 14:05:22 -0400

    ajustement des data du minizinc

--- solver.py
+++ solver.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import minizinc
 import pandas as pd
+import enum
 
 class Solver_ampl():
 
@@ -45,28 +46,43 @@ class Solver_mz():
 
         solver = minizinc.Solver.lookup('gecode')
 
+        #appel du modèle
         model_path = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2/tp2.mzn')
         problem = minizinc.Model(model_path)
 
-        # code
+        #création des data
         data_path = os.path.normpath('/Users/victorialemay/Documents/ecole/Genie_industriel/Session_6/SIAD/TP2')
-        #instance = minizinc.Instance(data_path)
-
         instance = minizinc.Instance(solver, problem)
-        instance['S'] = '{soir1, soir2, soir3, soir4, soir5, soir6, soir7, soir8, soir9, soir10, soir11}'
+        S = enum.Enum('S', ['soir1', 'soir2', 'soir3', 'soir4', 'soir5', 'soir6', 'soir7', 'soir8', 'soir9', 'soir10', 'soir11'])
+        instance['S'] = S
         
         if self.inst == 'inst1_dzn':
-            instance['Z'] = '{zone1, zone2, zone3, zone4, zone5}'
+            Z = enum.Enum('Z', ['zone1', 'zone2', 'zone3', 'zone4', 'zone5'])
+            instance['Z'] = Z
         
         data_path_inst = os.path.join(data_path, self.ins)
         
         data = pd.read_csv(data_path_inst, delimiter='=', header=None, names=['param', 'value'])
 
+        placedispo = data.loc[data['param'] == 'placedispo', 'value'].values[0]
+        cfixe = data.loc[data['param'] == 'cfixe', 'value'].values[0]
+        salaire = data.loc[data['param'] == 'salaire', 'value'].values[0]
+        esssence = data.loc[data['param'] == 'essence', 'value'].values[0]
+        vitbus = data.loc[data['param'] == 'vitbus', 'value'].values[0]
+        maxbus = data.loc[data['param'] == 'maxbus', 'value'].values[0]
+        pers = data.loc[data['param'] == 'pers', 'value'].values[0]
+        pourcentage = data.loc[data['param'] == 'pourcentage', 'value'].values[0]
+        distancet = data.loc[data['param'] == 'distancet', 'value'].values[0]
 
-
-
-        #instance = Instance(solver, problem)
-        #instance['maxbus'] = 5000
+        instance['placedispo'] = placedispo
+        instance['cfixe'] = cfixe
+        instance['salaire'] = salaire
+        instance['essence'] = esssence
+        instance['vitbus'] = vitbus
+        instance['maxbus'] = maxbus
+        instance['pers'] = pers
+        instance['pourcentage'] = pourcentage
+        instance['distancet'] = distancet
 
         result = solver.solve_all(problem, instance)
 
